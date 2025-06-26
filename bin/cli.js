10748c135d8cfa33a10d8c8ca3901fa17af5cc4c
xym009621 <122416270+xym009621@users.noreply.github.com>
2025-06-26 13:50:32 +0800

    Add files via upload

---
+++ bin/cli.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+const {program} = require('commander')
+// console.log('mycli');
+// if(process.argv[2]=='--help'){
+//     console.log('获取到了命令参数')
+// }
+const myhelp = require('../lib/core/help')
+myhelp(program)
+const mycomander = require('../lib/core/mycomander')
+mycomander(program)
+
+program.parse(process.argv)
