176bc8a0af6b12d4c3458e33622b68dd422b1b6d
xym009621 <122416270+xym009621@users.noreply.github.com>
2025-06-26 13:49:30 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    framework:['express','koa','egg'],
+    frameworkurl:{
+        express:'https://github.com/expressjs/express.git',
+        koa:'https://github.com/koajs/koa.git',
+        egg:'https://github.com/eggjs/egg.git'
+    }
+}
